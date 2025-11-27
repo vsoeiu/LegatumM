0f382feedd1e2f1b2fa80f88ef37d8374ea0803e
vsoeiu <adrianbarron352@gmail.com>
2025-11-27 12:45:26 -0600

    Version suputa madre

--- app.py
+++ app.py
@@ -483,8 +483,7 @@ def genres_view():
 def genre_detail_view(nombre_genero):
     data = logic.get_genre_collection(nombre_genero)
     return render_template('generos_detalle.html', genero=nombre_genero, artistas=data)
-'''
-if __name__ == '__main__':
-    print("🚀 LEGATUM SYSTEM v12.0 ONLINE - http://127.0.0.1:8080")
-    app.run(debug=True, port=8080)
-    '''
+
+#if __name__ == '__main__':
+# print("🚀 LEGATUM SYSTEM v12.0 ONLINE - http://127.0.0.1:8080")
+#  app.run(debug=True, port=8080)
